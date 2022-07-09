bead65d313d763f82011b6c5de11be8b485305a2
Omotolani Popoola <omotolanipopoola@DotPays-MacBook-Pro-2.local>
2022-07-09 19:59:14 +0100

    fifth commit

--- src/component/landingPage.jsx
+++ src/component/landingPage.jsx
@@ -3,12 +3,24 @@ import './landingPage.css'
 import Nav from './nav';
 import bg from '../asset/bg.jpeg';
 import HeroContent from './heroContent';
+import logo from '../asset/Logo.png'
+import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
+
+  let navigate = useNavigate()
+
   return (
     <>
         <div className='imageCover'>
-            <Nav />
+            <>
+            <nav className='navbar2'>
+              <div onClick={() => {navigate('/')}}>
+                  <img src={logo} alt="Nextflix Logo" className='title2'/>
+              </div>
+              <button onClick={() => {navigate('/signin')}} className='btn'>Sign Up</button>
+            </nav>
+            </>
             <img src={bg} alt="background" className='bg'/>
             <div className='gradient'></div>
             <div className='topContent'>
